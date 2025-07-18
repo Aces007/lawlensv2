df2f1d20af0c51b338983ece0cb815f18b12f0c2
Ace Lawrence Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-07-18 13:12:54 +0800

    navheader working w/tailwind

--- src/reusables/MobileHeader.jsx
+++ src/reusables/MobileHeader.jsx
@@ -1,8 +1,17 @@
-const MobileHeader = () => {
-    return (
-        <>
+import { Link } from "react-router-dom";
 
-        </>
+const MobileHeader = ({ onClose, isClosing }) => {
+    return (
+        <div>
+            <div className={`burger_menu ${isClosing ? "fadeOut" : "fadeIn"} `}>
+                <button className="burger_head_close" onClick={onClose}>✕</button>
+
+                <Link to="/about" className="mob_btns" onClick={onClose}>About</Link>
+                <Link to="/experience" className="mob_btns" onClick={onClose}>Experience</Link>
+                <Link to="/portfolio" className="mob_btns" onClick={onClose}>Portfolio</Link>
+                <Link to="/contact" className="mob_btns" onClick={onClose}>Contact</Link>
+            </div>
+        </div>
     )
 }
 
--- src/reusables/NavigationHeader.jsx
+++ src/reusables/NavigationHeader.jsx
@@ -4,56 +4,56 @@ import { useState, useEffect } from "react";
 import MobileHeader from "./MobileHeader";
 
 const NavigationHeader = () => {
-//   const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
-//   const [animateClose, setAnimateClose] = useState(false);
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
+  const [animateClose, setAnimateClose] = useState(false);
 
-//   const handleToggleMenu = () => {
-//     if (burgerMenuOpen) {
-//       setAnimateClose(true);
-//       setTimeout(() => {
-//         setBurgerMenuOpen(false);
-//         setAnimateClose(false);
-//       }, 800)
-//     } else {
-//       setBurgerMenuOpen(true);
-//     }
-//   };
+  const handleToggleMenu = () => {
+    if (burgerMenuOpen) {
+      setAnimateClose(true);
+      setTimeout(() => {
+        setBurgerMenuOpen(false);
+        setAnimateClose(false);
+      }, 800)
+    } else {
+      setBurgerMenuOpen(true);
+    }
+  };
 
-//   useEffect(() => {
-//     document.body.style.overflow = burgerMenuOpen ? 'hidden' : 'auto';
-//   }, [burgerMenuOpen])
+  useEffect(() => {
+    document.body.style.overflow = burgerMenuOpen ? 'hidden' : 'auto';
+  }, [burgerMenuOpen])
 
   
 
   return (
     <header>
       {/* Desktop & Tablet Header */}
-      <div className="fixed flex flex-row items-center justify-around top-0 left-0 w-full z-50 py-8">
+      <div className="hidden md:flex fixed flex-row items-center justify-around top-0 left-0 w-full z-50 py-8">
         <div className="flex flex-row items-center justify-around w-screen mx-20">
           
-          <div className="flex flex-row ">
-            <Link to="/about" className="nav_btns">About</Link>
-            <Link to="/experience" className="nav_btns">Experience</Link>
+          <div className="flex flex-row justify-between items-center gap-20">
+            <Link to="/about" className="font-nunito font-bold uppercase text-primary text-buttons">About</Link>
+            <Link to="/experience" className="font-nunito font-bold uppercase text-primary text-buttons">Experience</Link>
           </div>
 
           <Link to="/"><img src={logo} alt="logo" className="logo_head" /></Link>
           
-          <div className="nav_head">
-            <Link to="/portfolio" className="nav_btns">Portfolio</Link>
-            <Link to="/contact" className="nav_btns">Contact</Link>
+          <div className="flex flex-row justify-between items-center gap-20">
+            <Link to="/portfolio" className="font-nunito font-bold uppercase text-primary text-buttons">Portfolio</Link>
+            <Link to="/contact" className="font-nunito font-bold uppercase text-primary text-buttons">Contact</Link>
           </div>
         </div>
       </div>
 
       {/* Mobile Header */}
-      {/* <div className="mobile_head">
+      <div className="mobile_head md:hidden">
         <img src={logo} alt="logo" className="logo_head_mob" />
         <button className="burger_head" onClick={handleToggleMenu}>☰</button>
       </div>
 
       {(burgerMenuOpen || animateClose) && (
         <MobileHeader onClose={handleToggleMenu} isClosing={animateClose}/>
-      )} */}
+      )}
     </header>
   );
 };
