ca2d417b2d097c92400e16d0e7d4b78b120c7ade
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-08-01 11:42:16 +0800

    started on the experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,4 +1,23 @@
+import React, {useEffect, useRef, useState} from "react";
+
 const Experience = () => {
+    const experiencesByYear = [
+        {
+            year: 2025,
+            experiences: [
+                {title: "Published ArchiTech Vault", image: './exp_content/ATV.png'},
+            ],
+        },
+    ]
+
+    const [activeIdx, setActiveIdx] = useState(0);
+    const [cardsVisible, setCardsVisible] = useState(false);
+    const yearRef = useRef(null);
+    const [yearStyle, setYearStyle] = useState( { transform: "translateY(0px) scale(1)" } )
+    
+    const {year, experiences } = experiencesByYear[activeIdx];
+
+
     return (
         <>
         </>
