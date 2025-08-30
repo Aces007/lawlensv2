91a23de966ddd156d4dabef17161885f2f982037
Ace Lawrence Zuñiga Clavano <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2025-08-30 14:41:54 +0800

    modifying page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+/* Pages Import */
 import Home from './pages/Home';
 import About from './pages/About';
 import Experience from './pages/Experience';
@@ -8,19 +11,30 @@ import NavigationHeader from './reusables/NavigationHeader';
 import './index.css';
 import './pages.scss';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+  
+  return (
+    <Router>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+    </Router>
+  );
+}
+
 
 function App() {
   return (
     <Router>
-      <NavigationHeader />
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/experience' element={<Experience />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <NavigationHeader />  
+      <AnimatedRoutes />
     </Router>
   );
 }
