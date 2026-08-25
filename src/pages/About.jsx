9d2434b9b05ba607e8fac8a8841a6862ad0a9226
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-08-25 15:23:56 +0800

    begin syntax for about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,17 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const About = () => {
+    // -- CONTAINERS AND ELEMENTS -- //
+    const about_container = "";
+
+
+    return (
+        <>
+            <div className={about_container}>
+                
+            </div>
+        </>
+    )
 }
 
 export default About;
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion";
 
 const Hero = () => {
+    // -- CONTAINERS AND ELEMENTS -- //
     const hero_container = "relative w-full h-[100dvh] grid grid-cols-1 grid-rows-1 place-items-center overflow-hidden select-none px-4";
     const hero_image_cont = "z-10 hidden resMd:flex resLg:flex resLg:relative resLg:bottom-[-230px] resLg:left-[-20px] justify-center items-end h-full w-full pointer-events-none pb-0";
     
     const hero_heading = "z-0 font-montserrat font-black uppercase tracking-[8px] text-white/80 resSm:text-[12vw] resMd:text-[16vw] resLg:text-[15vw] leading-none text-center pointer-events-none whitespace-nowrap";
     const hero_image = "h-[75vh] resMd:h-[78vh] resLg:h-[120vh] w-auto max-w-none object-contain";
+
+    
   return (
     <div className={hero_container}>
       <motion.h1
