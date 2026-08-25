0ddf0a147da54a1efb5b71d50a6e74a2b7b66932
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-08-25 16:18:00 +0800

    installed swiper js

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' 
-import './pages.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+import './index.css';
+import './pages.scss';
+
+// -- Swiper Modules -- //
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import 'swiper/css/effect-cards';
+import 'swiper/css/effect-coverflow';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards, Autoplay } from "swiper/modules";
 import { motion } from "framer-motion";
+
+const polaroids = [
+    { id: 1, title: "", rotate: "-rotate-6", image: ""},
+    { id: 2, title: "", rotate: "-rotate-2", image: ""},
+    { id: 3, title: "", rotate: "rotate-0", image: ""},
+    { id: 4, title: "", rotate: "rotate-3", image: ""},
+    { id: 5, title: "", rotate: "rotate-6", image: ""},
+];
+
 
 const About = () => {
     // -- CONTAINERS AND ELEMENTS -- //
-    const about_container = "";
+    const about_container = "w-full h-full min-h-[100dvh] flex flex-col justify-between items-center px-6 resMd:px-12 resLg:px-16 py-10 select-none overflow-hidden max-w-6xl mx-auto";
 
 
     return (
